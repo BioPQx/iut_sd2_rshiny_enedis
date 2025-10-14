b8351ee1ca2e0dd2788f68e0e7f8cf8447413f2d
BioPQx <152429189+BioPQx@users.noreply.github.com>
2025-10-14 10:12:57 +0200

    Update requete_ademe.R

--- requete_ademe.R
+++ requete_ademe.R
@@ -5,18 +5,15 @@ library(jsonlite)
 
 base_url <- "https://data.ademe.fr/data-fair/api/v1/datasets/dpe03existant/lines"
 
-# Initialiser un dataframe vide
 final_df <- data.frame()
-
-# Initialiser la page
 page <- 1
 
 repeat {
-  # Paramètres de la requête
   params <- list(
     page = page,
     size = 5,
-    select = "nom_commune_ban",
+    select = paste(
+      "nom_commune_ban",
       "etiquette_ges",
       "code_departement_ban",
       "annee_construction",
@@ -28,32 +25,24 @@ repeat {
       "surface_habitable_logement",
       "hauteur_sous_plafond",
       "etiquette_dpe",
-    qs = 'code_departement_ban : "01"  OR code_departement_ban : "30"'
+      sep = ","
+    ),
+    qs = 'code_departement_ban : "01" OR code_departement_ban : "30"'
   )
   
-  # Encodage de l'URL avec paramètres
   url_encoded <- modify_url(base_url, query = params)
-  
-  # Effectuer la requête GET
   response <- GET(url_encoded)
+  stop_for_status(response)
   
-  # Extraire le contenu JSON et convertir en dataframe
   content <- fromJSON(rawToChar(response$content), flatten = TRUE)
   df <- content$results
   
-  # Ajouter les résultats au dataframe final
   final_df <- rbind(final_df, df)
   
-  # Si moins de 5 résultats renvoyés, fin de la boucle
-  if (nrow(df) < 5) {
-    break
-  }
-  
-  # Page suivante
+  if (nrow(df) < 5) break
   page <- page + 1
+  Sys.sleep(0.2)  # sécurité pour éviter de spammer l'API
 }
 
-# Afficher la dimension du dataframe final
 dim(final_df)
-# Afficher les données
 View(final_df)
